6149184e46619c517b148996639786f3788fdb8f
Rebecca <rebecca@heyman.com>
2024-04-15 16:15:50 -0500

    convert standard css to tailwind

--- personal_portfolio_website/app/components/Navbar.jsx
+++ personal_portfolio_website/app/components/Navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar() {
       <Link href="/">Home</Link>
       <Link href="/about">About Me</Link>
       <Link target="_blank" href="https://github.com/kawaiimusician" passHref={true}>Github</Link>
+      <Link href="/blog">Blog</Link>
       <Link href="/music">Music</Link>
       <Link href="/cat_pics">Cats</Link>
     </nav>
--- personal_portfolio_website/tailwind.config.js
+++ personal_portfolio_website/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     extend: {
       colors: {
         primary: '#14000c',
+        secondary: '#3b0123',
         offWhite: '#e0e0e0'
+      },
+      flex: {
+        '20': '20'
       }
     },
   },
