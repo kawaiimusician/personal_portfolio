8d03106281653ffba0a0fd4b3ed575487c44744d
Rebecca <rebecca@heyman.com>
2024-04-16 18:45:14 -0500

    remove useless pages

--- personal_portfolio_website/app/about/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <main className="">
-      <h2>About Me!</h2>
-    </main>
-  );
-}
--- personal_portfolio_website/app/components/Navbar.jsx
+++ personal_portfolio_website/app/components/Navbar.jsx
@@ -6,11 +6,8 @@ export default function Navbar() {
       <p>Rebecca Heyman</p>
       <div className="spacer"></div>
       <Link href="/">Home</Link>
-      <Link href="/about">About Me</Link>
       <Link target="_blank" href="https://github.com/kawaiimusician" passHref={true}>Github</Link>
       <Link href="/blog">Blog</Link>
-      <Link href="/music">Music</Link>
-      <Link href="/cat_pics">Cats</Link>
     </nav>
   )
 }
